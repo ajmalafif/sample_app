f6fc3fcef0f3c22d6689b5608d62e261ff54715d
Ajmal Afif <ajmalafif@gmail.com>
2011-09-02 12:32:27 +0800

    tried to make spork and autotest to work

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,30 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module SampleApp
   class Application < Rails::Application
+    .
+    .
+    .
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+        end
+      end
+    end
+  end
+
+
+## require File.expand_path('../boot', __FILE__)
+
+## require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+## Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+## module SampleApp
+  ## class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -34,9 +58,9 @@ module SampleApp
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+  ##  config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-  end
-end
+  ##  config.filter_parameters += [:password]
+  ## end
+## end
