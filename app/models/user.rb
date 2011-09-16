7080a52eb938f8d0902e1bfb55f481ff525897a2
Ajmal Afif <ajmalafif@gmail.com>
2011-09-16 15:33:55 +0800

    length and format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
-class User < ActiveRecord::Base
-end
-
 # == Schema Information
 #
 # Table name: users
@@ -11,4 +8,13 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates  :email, :presence => true
+end
+
 
