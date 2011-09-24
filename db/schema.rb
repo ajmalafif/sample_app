efabfbb7ae0789a7ec29f6abb93e8375104e6bd2
Ajmal Afif <ajmalafif@gmail.com>
2011-09-24 13:56:58 +0800

    Added password and encrypted_password attribute to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110917045725) do
+ActiveRecord::Schema.define(:version => 20110924055113) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
