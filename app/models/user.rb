7bb50dcd6969da9a7b91e8bc34078372e4be4c0f
Ajmal Afif <ajmalafif@gmail.com>
2011-10-09 14:09:06 +0800

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,9 +24,13 @@ class User < ActiveRecord::Base
   class << self 
     def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil   if user.nil?
-      return user  if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
     end 
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
   end
   
   private
@@ -51,6 +55,7 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -61,5 +66,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
