70ac3c3a7da101f369f7cf96abff98b85b3b900a
Ajmal Afif <ajmalafif@gmail.com>
2011-10-22 02:15:36 +0800

    Can now destroy users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    redirect_to users.path, :flash => { :success => "User destroyed." }
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
 
   private
